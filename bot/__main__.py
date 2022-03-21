c6c98c04801550c84dfaf3647347b479780493d1
Rahman510 <72034959+Rahman510@users.noreply.github.com>
2022-03-21 10:43:36 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -154,9 +154,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='Slam Mirrorbot Help',
-        author_name='Slam Mirrorbot',
-        author_url='https://github.com/SlamDevs/slam-mirrorbot',
+        title='Gawr Mirror Help',
+        author_name='Gawr Mirror',
+        author_url='https://t.me/gawrmirror',
         html_content=help_string_telegraph,
     )["path"]
 
